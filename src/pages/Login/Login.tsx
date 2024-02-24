95e90fb0721720ea5104d9049da69b33020d9591
azizbg1 <aziz.benguirat@esprit.tn>
2024-02-24 18:31:33 +0100

    final

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './Login.css'
+import { z } from 'zod'; // Importez Zod
+
 
 import email_icon from '../../assets/img/email.png'
 import password_icon from '../../assets/img/password.png'
-import signup_icon from '../../assets/img/login.png'
+import signup_icon from '../../assets/img/aziz3.jpg'
 import logo from '../../assets/img/logo.png'
 import google_icon from '../../assets/img/login3.png'
 
@@ -12,7 +14,14 @@ import google_icon from '../../assets/img/login3.png'
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState<{email?: string;}>({}); // Définir le type de errors
+  const [isSubmitted, setIsSubmitted] = useState(true); // Initialise à true pour désactiver initialement le bouton
 
+
+
+  const signupSchema = z.object({
+    email: z.string().email(),
+  });
 
 
  
@@ -24,15 +33,42 @@ export default function Login() {
   };
   const isFormValid = email !== '' && password !== '';
 
+  
   const handleSubmit = () => {
-   
-    if (email.trim() === '' || password.trim() === '') {
-      alert('Veuillez remplir tous les champs du formulaire.');
-    } else {
-      console.log(email, password);
-    }
-  };
+    setIsSubmitted(true);
 
+    if (isFormValid) {
+      
+    const userData = {
+      email: email.trim(),
+      password: password.trim(),
+    };
+  
+    try {
+      // Validez les données avec le schéma Zod
+      signupSchema.parse(userData);
+      // Si la validation réussit, soumettez le formulaire ou appelez une API ici
+      console.log("Form data:", userData);
+      setErrors({});
+  
+    } catch (error) {
+      // S'il y a des erreurs de validation, mettez à jour l'état des erreurs
+      if (error instanceof z.ZodError) {
+        const fieldErrors: { [key: string]: string } = {};
+        error.errors.forEach((err) => {
+          fieldErrors[err.path[0] as string] = err.message;
+        });
+        setErrors(fieldErrors);
+      }
+    }
+  }
+};
+
+const errorMessage = !isFormValid && isSubmitted && (
+  <div className="error">Please fill in all the required fields.</div>
+);
+
+const buttonClass = `submit ${!isFormValid && isSubmitted ? 'disabled' : ''}`;
 
 
   return (
@@ -49,10 +85,11 @@ export default function Login() {
     <div className="inputs">
       
 
-      <div className="input">
-      <img src={email_icon} alt="" /> 
-      <input type="email" placeholder='Email' value={email} onChange={handleChangeemail}  />
-      </div>
+    <div className="input">
+            <img src={email_icon} alt="" /> 
+            <input type="email" placeholder='Email' value={email} onChange={handleChangeemail} />
+          </div>
+          {errors.email && <div className="error">{errors.email}</div>}
 
       <div className="input">
       <img src={password_icon} alt="" /> 
@@ -69,14 +106,20 @@ export default function Login() {
       </div>
       <div className="forgot-password">Lost password ? <span>Click here !</span></div>
       <div className="submit-container">
-        <Link to="/signup" className="submit1">Sign up</Link> {/* Redirection vers la page de connexion */}
-        <div className={`submit ${isFormValid ? '' : 'disabled'}`} onClick={isFormValid ? handleSubmit : null}>Login</div>
-
-
-        
+        {/* Bouton "Sign up" */}
+        <Link to="/signup" className={`submit1 ${isFormValid ? '' : 'disabled'}`}>Sign up</Link>
+        {/* Bouton "Login" */}
+        <div className={buttonClass} onClick={isFormValid ? handleSubmit : null} style={!isFormValid && isSubmitted ? { cursor: 'not-allowed' } : {}}>
+    Login
+  </div>
 
       </div>
-      </div>
+     
+
+      {/* Affichez le message d'erreur */}
+      {errorMessage}
+    </div>
+    
       <div className="right-half">
         {/* Ajoutez votre image de profil ici */}
         <img src={signup_icon} alt="Profile" />
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,137 +1,133 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import SignIn from './SignIn'; // Importez votre composant SignIn
-import { signupSchema } from '../../schema/SignupSchema'; // Importez votre schéma de validation
-import './signup.css'
 import { z } from 'zod'; // Importez Zod
+import './signup.css';
 
-
-import user_icon from '../../assets/img/person.png'
-import email_icon from '../../assets/img/email.png'
-import password_icon from '../../assets/img/password.png'
-import signup_icon from '../../assets/img/signup2.png'
-import google_icon from '../../assets/img/login3.png'
-import logo from '../../assets/img/logo.png'
-
-
-
+import user_icon from '../../assets/img/person.png';
+import email_icon from '../../assets/img/email.png';
+import password_icon from '../../assets/img/password.png';
+import signup_icon from '../../assets/img/aziz1.jpg';
+import google_icon from '../../assets/img/login3.png';
+import logo from '../../assets/img/logo.png';
 
 export default function Signup() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [errors, setErrors] = useState<{ name?: string; email?: string; password?: string }>({}); // Définir le type de errors
-
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<{ name?: string; email?: string; password?: string }>({});
+  const [isSubmitted, setIsSubmitted] = useState(true);
 
   const signupSchema = z.object({
-    name: z.string().min(2).max(50),
+    name: z.string()
+      .min(2)
+      .max(50)
+      .regex(/^[a-zA-Z]+$/, { message: "Name must contain only letters." }),
     email: z.string().email(),
-    password: z.string().min(6),
+    password: z.string()
+      .min(6)
+      .regex(/[0-9]/, { message: "Password must contain at least one digit." })
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: "Password must contain at least one special character." }),  
   });
+  
 
   const handleChangename = (e) => {
     setName(e.target.value);
   };
+
   const handleChangeemail = (e) => {
     setEmail(e.target.value);
   };
+
   const handleChangepassword = (e) => {
     setPassword(e.target.value);
   };
+
   const isFormValid = name !== '' && email !== '' && password !== '';
 
   const handleSubmit = () => {
-    const userData = {
-      name: name.trim(),
-      email: email.trim(),
-      password: password.trim(),
-    };
-  
-    try {
-      // Validez les données avec le schéma Zod
-      signupSchema.parse(userData);
-      // Si la validation réussit, soumettez le formulaire ou appelez une API ici
-      console.log("Form data:", userData);
-      setErrors({});
-  
-    } catch (error) {
-      // S'il y a des erreurs de validation, mettez à jour l'état des erreurs
-      if (error instanceof z.ZodError) {
-        const fieldErrors: { [key: string]: string } = {};
-        error.errors.forEach((err) => {
-          fieldErrors[err.path[0] as string] = err.message;
-        });
-        
+    setIsSubmitted(true);
 
-        
-        setErrors(fieldErrors);
+    if (isFormValid) {
+      const userData = {
+        name: name.trim(),
+        email: email.trim(),
+        password: password.trim(),
+      };
+
+      try {
+        signupSchema.parse(userData);
+        console.log("Form data:", userData);
+        setErrors({});
+      } catch (error) {
+        if (error instanceof z.ZodError) {
+          const fieldErrors: { [key: string]: string } = {};
+          error.errors.forEach((err) => {
+            fieldErrors[err.path[0] as string] = err.message;
+          });
+          setErrors(fieldErrors);
+        }
       }
     }
   };
-  
 
+  const errorMessage = !isFormValid && isSubmitted && (
+    <div className="error">Please fill in all the required fields.</div>
+  );
 
+const buttonClass = `submit ${!isFormValid && isSubmitted ? 'disabled' : ''}`;
 
   return (
     <>
-      <div className='container '>
-      <div className="left-half">
+      <div className='container'>
+        <div className="left-half">
+          <div className="header">  
+            <img src={logo} alt="Logo" style={{ width: '200px', height: 'auto' }} />
+            <div className="text">Sign up</div>
+            <div className="underline"></div>  
+          </div>
+          <div className="inputs">
+            <div className="input">
+              <img src={user_icon} alt="" /> 
+              <input type="text" placeholder='Name' value={name} onChange={handleChangename} />
+            </div>
+            {errors.name && <div className="error">{errors.name}</div>}
 
-      <div className="header">  
-      <img src={logo} alt="Logo" style={{ width: '200px', height: 'auto' }} /> {/* Ajoutez votre logo ici avec la taille désirée */}
-      <div className="text">Sign up</div>
-      <div className="underline"></div>  
-    </div>
-    <div className="inputs">
-      <div className="input">
-      <img src={user_icon} alt="" /> 
-      <input type="text" placeholder='Name' value={name} onChange= {handleChangename}  />
-      {errors.name && <span className="error">{errors.name}</span>}
+            <div className="input">
+              <img src={email_icon} alt="" /> 
+              <input type="email" placeholder='Email' value={email} onChange={handleChangeemail} />
+            </div>
+            {errors.email && <div className="error">{errors.email}</div>}
 
-      
+            <div className="input">
+              <img src={password_icon} alt="" /> 
+              <input type="password" placeholder='Password' value={password} onChange={handleChangepassword} />
+            </div>
+            {errors.password && <div className="error">{errors.password}</div>}
+          </div>
+          
+          <div>
+            <div className="header"> 
+              <div className="text2">         </div>
+              <div className="text2">         </div>
+              <div className="text2">         </div>
+              <div className="underline"></div>  
+            </div>  
+          </div>  
+
+          <div className="submit-container">
+  <div className={buttonClass} onClick={isFormValid ? handleSubmit : null} style={!isFormValid && isSubmitted ? { cursor: 'not-allowed' } : {}}>
+    Sign up
+  </div>
+  <Link to="/login" className="submit1">Login</Link> {/* Redirection vers la page de connexion */}
+</div>
+          {/* Affichez le message d'erreur */}
+          {errorMessage}
+        </div>
+        <div className="right-half">
+          {/* Ajoutez votre image de profil ici */}
+          <img src={signup_icon} alt="Profile" />
+        </div>
       </div>
-
-      <div className="input">
-      <img src={email_icon} alt="" /> 
-      <input type="email" placeholder='Email' value={email} onChange={handleChangeemail}  />
-      {errors.email && <span className="error">{errors.email}</span>}
-
-      </div>
-
-      <div className="input">
-      <img src={password_icon} alt="" /> 
-      <input type="password" placeholder='Password' value={password} onChange={handleChangepassword}  />
-      {errors.password && <span className="error">{errors.password}</span>}
-
-      </div>
-      </div>
-      
-      <div>
-      <div className="header"> 
-      <div className="text2">         </div>
-      <div className="text2">         </div>
-      <div className="text2">         </div>
-       <div className="underline"></div>  
-      </div>  
-      </div>  
-
-
-
-      <div className="submit-container">
-      <div className={`submit ${isFormValid ? '' : 'disabled'}`} onClick={isFormValid ? handleSubmit : null}>Sign up</div>
-        <Link to="/login" className="submit1">Login</Link> {/* Redirection vers la page de connexion */}
-
-        
-
-      </div>
-      </div>
-      <div className="right-half">
-        {/* Ajoutez votre image de profil ici */}
-        <img src={signup_icon} alt="Profile" />
-      </div>
-      </div>
-
-      
     </>
   );
-} 
+}
