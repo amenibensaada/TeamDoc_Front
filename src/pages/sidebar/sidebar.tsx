9f68d58082ecd71ff0aa073d6935ff3fc0984b55
Hana-Romdhani <romdhani.hana@esprit.tn>
2024-02-27 21:45:45 +0100

    update

--- src/pages/sidebar/sidebar.tsx
+++ src/pages/sidebar/sidebar.tsx
@@ -1,8 +1,7 @@
-import { useState, useEffect} from "react";
+import { useState} from "react";
 
 export default function SideBar() {
     const [open, setOpen] = useState(true);
-    const [showButton, setShowButton] = useState(false);
 
     const Menus = [
       { title: "Dashboard", src: "Chart_fill" },
@@ -22,15 +21,7 @@ export default function SideBar() {
       { title: "logout", src: "logout" },
     ];
    
-    useEffect(() => {
-      const handleResize = () => {
-        setShowButton(window.innerWidth < 500);
-      };
   
-      handleResize(); // Check initial width
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
-    }, []);
     return (
         <div className="flex">
           <div
