ae30c193c7051c7cd41e421fd35939b6e421b239
Hana-Romdhani <romdhani.hana@esprit.tn>
2024-04-19 20:24:46 +0100

    update

--- src/pages/home/staticlist.tsx
+++ src/pages/home/staticlist.tsx
@@ -21,19 +21,6 @@ const StaticFileList = () => {
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
-     // const [searchTerm, setSearchTerm] = useState(""); // État pour stocker le terme de recherche
-  // const [sortByName, setSortByName] = useState(false);
-<!--   const [searchTerm] = useState(""); // État pour stocker le terme de recherche
-  const [sortByName] = useState(false); // État pour activer/désactiver le tri par nom
-  const [currentPage, setCurrentPage] = useState(1); // État pour stocker le numéro de la page actuelle
-  const itemsPerPage = 5; // Nombre d'éléments par page -->
-
-  // Fonction pour mettre à jour le terme de recherche
-  // const handleSearchChange = (e: {
-  //   target: { value: React.SetStateAction<string> };
-  // }) => {
-  //   setSearchTerm(e.target.value);
-  // };
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -85,9 +72,7 @@ const StaticFileList = () => {
       <div className="main-content">
         <div className="header">
           <div className="pp">
-<!--              <div className="main-content2">
-        <div className="header1">
-          <div className="pp1"> -->
+           
             <h1>Bienvenue sur TeamDoc</h1>
 
             <img
@@ -141,16 +126,6 @@ const StaticFileList = () => {
 
         </div>
         
-        
-<!--           </div>
-        </div> -->
-        {/* <button onClick={() => setSortByName(!sortByName)}>
-              {sortByName
-                ? "Désactiver le tri par nom"
-                : "Activer le tri par nom"}
-            </button>
-          </div>
-        </div> */}
         <div className="file-list-container">
           <div className="static-file-list">
             {currentItems.map((file) => (
