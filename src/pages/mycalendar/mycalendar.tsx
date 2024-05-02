828e5345cbbb1a6f966c91755eca215d01114ed6
amenibensaada <amenisaada19@gmail.com>
2024-05-02 19:19:49 +0100

    fix

--- src/pages/mycalendar/mycalendar.tsx
+++ src/pages/mycalendar/mycalendar.tsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import Modal from 'react-modal';
-import './modal.css'; // Import des styles CSS personnalisés
+import React, { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import Modal from "react-modal";
+import "./modal.css";
 
 const localizer = momentLocalizer(moment);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const MyCalendar = (props: any) => {
+  console.log(props);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
-  const [reminderText, setReminderText] = useState('');
+  const [reminderText, setReminderText] = useState("");
 
   const events = [
     {
       id: 1,
-      title: 'Meeting',
+      title: "Meeting",
       start: new Date(2024, 4, 1, 10, 0),
       end: new Date(2024, 4, 1, 12, 0),
     },
     {
       id: 2,
-      title: 'Conference',
+      title: "Conference",
       start: new Date(2024, 4, 10, 13, 0),
       end: new Date(2024, 4, 10, 17, 0),
     },
   ];
 
-  const handleSelectSlot = (slotInfo: { start: React.SetStateAction<null>; }) => {
+  const handleSelectSlot = (slotInfo: {
+    start: React.SetStateAction<null>;
+  }) => {
     setSelectedDate(slotInfo.start);
     setModalIsOpen(true);
   };
@@ -38,13 +42,18 @@ const MyCalendar = (props: any) => {
 
   const handleReminderSubmit = () => {
     // Logique pour enregistrer le rappel avec reminderText
-    console.log('Rappel enregistré pour le', selectedDate, 'avec le texte:', reminderText);
+    console.log(
+      "Rappel enregistré pour le",
+      selectedDate,
+      "avec le texte:",
+      reminderText
+    );
     setModalIsOpen(false);
-    setReminderText(''); // Réinitialiser le texte du rappel après la fermeture de la fenêtre modale
+    setReminderText(""); // Réinitialiser le texte du rappel après la fermeture de la fenêtre modale
   };
 
   return (
-    <div style={{ height: 500, position: 'relative' }}>
+    <div style={{ height: 500, position: "relative" }}>
       <Calendar
         localizer={localizer}
         events={events}
@@ -52,15 +61,23 @@ const MyCalendar = (props: any) => {
         endAccessor="end"
         selectable
         onSelectSlot={handleSelectSlot}
-        style={{ maxWidth: '800px', margin: '0 auto' }}
+        style={{ maxWidth: "800px", margin: "0 auto" }}
       />
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="modal-content" overlayClassName="modal-overlay">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className="modal-content"
+        overlayClassName="modal-overlay">
         <div className="modal-header">
           <h2 className="modal-title">Enregistrer un rappel</h2>
-          <button onClick={closeModal} className="close-button">X</button>
+          <button onClick={closeModal} className="close-button">
+            X
+          </button>
         </div>
         <div className="modal-body">
-          <h3 className="date-title">Pour le {selectedDate && moment(selectedDate).format('LL')}</h3>
+          <h3 className="date-title">
+            Pour le {selectedDate && moment(selectedDate).format("LL")}
+          </h3>
           <input
             type="text"
             placeholder="Entrez votre rappel ici"
@@ -70,8 +87,12 @@ const MyCalendar = (props: any) => {
           />
         </div>
         <div className="modal-footer">
-          <button onClick={handleReminderSubmit} className="modal-button">Enregistrer</button>
-          <button onClick={closeModal} className="modal-button">Annuler</button>
+          <button onClick={handleReminderSubmit} className="modal-button">
+            Enregistrer
+          </button>
+          <button onClick={closeModal} className="modal-button">
+            Annuler
+          </button>
         </div>
       </Modal>
     </div>
