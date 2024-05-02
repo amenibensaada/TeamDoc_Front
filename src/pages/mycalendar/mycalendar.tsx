2d67c65f8487b6951ed1267cfd5d13fff0f41781
amenibensaada <amenisaada19@gmail.com>
2024-05-02 19:38:56 +0100

    fix pipeline

--- src/pages/mycalendar/mycalendar.tsx
+++ src/pages/mycalendar/mycalendar.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import Modal from "react-modal";
 import "./modal.css";
+import SideBar from "../sidebar/sidebar";
 
 const localizer = momentLocalizer(moment);
 
@@ -11,7 +12,7 @@ const localizer = momentLocalizer(moment);
 const MyCalendar = (props: any) => {
   console.log(props);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [reminderText, setReminderText] = useState("");
 
   const events = [
@@ -29,9 +30,7 @@ const MyCalendar = (props: any) => {
     },
   ];
 
-  const handleSelectSlot = (slotInfo: {
-    start: React.SetStateAction<null>;
-  }) => {
+  const handleSelectSlot = (slotInfo: { start: Date }) => {
     setSelectedDate(slotInfo.start);
     setModalIsOpen(true);
   };
@@ -53,48 +52,52 @@ const MyCalendar = (props: any) => {
   };
 
   return (
-    <div style={{ height: 500, position: "relative" }}>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        selectable
-        onSelectSlot={handleSelectSlot}
-        style={{ maxWidth: "800px", margin: "0 auto" }}
-      />
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        className="modal-content"
-        overlayClassName="modal-overlay">
-        <div className="modal-header">
-          <h2 className="modal-title">Enregistrer un rappel</h2>
-          <button onClick={closeModal} className="close-button">
-            X
-          </button>
-        </div>
-        <div className="modal-body">
-          <h3 className="date-title">
-            Pour le {selectedDate && moment(selectedDate).format("LL")}
-          </h3>
-          <input
-            type="text"
-            placeholder="Entrez votre rappel ici"
-            className="modal-input"
-            value={reminderText}
-            onChange={(e) => setReminderText(e.target.value)}
-          />
-        </div>
-        <div className="modal-footer">
-          <button onClick={handleReminderSubmit} className="modal-button">
-            Enregistrer
-          </button>
-          <button onClick={closeModal} className="modal-button">
-            Annuler
-          </button>
-        </div>
-      </Modal>
+    <div className="grid min-h-screen w-full grid-cols-[240px_1fr] overflow-hidden">
+      <SideBar />
+      <div style={{ height: 500, position: "relative" }}>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          selectable
+          onSelectSlot={handleSelectSlot}
+          className=" mx-60 my-40"
+          style={{ maxWidth: "800px" }}
+        />
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          className="modal-content"
+          overlayClassName="modal-overlay">
+          <div className="modal-header">
+            <h2 className="modal-title">Enregistrer un rappel</h2>
+            <button onClick={closeModal} className="close-button">
+              X
+            </button>
+          </div>
+          <div className="modal-body">
+            <h3 className="date-title">
+              Pour le {selectedDate && moment(selectedDate).format("LL")}
+            </h3>
+            <input
+              type="text"
+              placeholder="Entrez votre rappel ici"
+              className="modal-input"
+              value={reminderText}
+              onChange={(e) => setReminderText(e.target.value)}
+            />
+          </div>
+          <div className="modal-footer">
+            <button onClick={handleReminderSubmit} className="modal-button">
+              Enregistrer
+            </button>
+            <button onClick={closeModal} className="modal-button">
+              Annuler
+            </button>
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 };
